012f8f3171eb9d1f9a3abddae81eed477bef6410
666lavendermilkshakes <nkrt2mmf7t@privaterelay.appleid.com>
2026-07-12 17:28:46 +0000

    текст комментария

--- fit_life.py
+++ fit_life.py
@@ -7,9 +7,21 @@ ML_PER_L = 1000
 print('Добро пожаловать в FitLife!')
 
 user_name = input('Ваше имя:').title()
-user_age = int(input('Ваш возраст:'))
-user_weight = float(input('Ваш вес:'))
-user_height = float(input('Ваш рост в метрах, используя точку (1.75):'))
+user_age = int(input('Ваш полный возраст:'))
+
+user_weight = input('Ваш вес в кг, используя точку (62.7):')
+if ',' in user_weight:
+    new_user_weight = user_weight.replace(',', '.')
+    user_weight = float(new_user_weight)
+else:
+    user_weight = float(user_weight)
+
+user_height = input('Ваш рост в метрах, используя точку (1.75):')
+if ',' in user_height:
+    new_user_height = user_height.replace(',', '.')
+    user_height = float(new_user_height)
+else:
+    user_height = float(user_height)
 
 # расчёт индекса массы тела
 bmi = user_weight / (user_height ** 2)
